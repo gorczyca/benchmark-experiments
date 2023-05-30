5a9363a347ce3980e6a42271d91aa8c62f8927f8
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2023-05-30 14:51:29 +0200

    corrected flexaspic

---
+++ misc/analyze_correctness.py
@@ -0,0 +1,40 @@
+import os
+
+import pandas as pd
+
+
+OUTPUTS_PATH = '/home/piotr/Dresden/aspic-flexdds/exp_outs_bak'
+
+CORRECT_OUTPUT = 'exp_1_adm_600_aspforaba.csv'
+
+
+if __name__ == '__main__':
+    outputs = {}
+
+    for file_name in os.listdir(OUTPUTS_PATH):
+        file_path = os.path.join(OUTPUTS_PATH, file_name)
+        if os.path.isfile(file_path):
+            outputs[file_name] = pd.read_csv(file_path)
+            outputs[file_name].set_index(['instance','goal'], inplace=True)
+            outputs[file_name]['verdict'] = None
+    
+    correct_df = outputs[CORRECT_OUTPUT]
+
+    for solver, df in outputs.items():
+        for correct_index, correct_row in correct_df.iterrows():
+            df_result = df.loc[correct_index, 'result']
+            if df_result == 'TIMEOUT':
+                verdict = None
+            else:
+                verdict = 'corr' if df.loc[correct_index, 'result'] == correct_row.result else 'inc'
+            df.loc[correct_index, 'verdict'] = verdict
+
+        df.to_csv(f'{OUTPUTS_PATH}/corr_{solver}')
+            
+    
+
+
+        
+
+
+
